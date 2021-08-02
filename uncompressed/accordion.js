6f3540882e9ee0b1e33ebaa6f8f9e395a6223b65
buppagi <buppagistar@gmail.com>
2021-08-03 00:16:36 +0900

    앞으로 추가할 내용 및 일부 내용 수정

--- uncompressed/accordion.js
+++ uncompressed/accordion.js
@@ -10,41 +10,74 @@
         $trigger = $selector.find('.' + settings.triggerClass),
         $panel = $selector.find('.' + settings.panelClass),
         $heading = $selector.find('.' + settings.headingClass),
-        activate = settings.activeClass,
+        activeClass = settings.activeClass,
         labelledby = $trigger.attr('id');
+
+      var uniqueId = $.fn.extend({
+        uniqueId: (function(){
+          var uuid = 0;
+
+          return function() {
+            return this.each(function(){
+              if ( !this.id ) {
+                this.id = "ui-id-" + ( ++uuid);
+              }
+            });
+          };
+
+        })(),
+
+        removeUniqueId: function(){
+          return this.each(function(){
+            if ( /^ui-id-\d+$/.test( this.id ) ) {
+              $( this ).removeAttr( "id" );
+            }
+          });
+        }
+      });
+
 
       var create = function () {
         $selector.attr('role', 'presentation');
+        var controls = $panel.attr('id');
         // heading 태그 체크
-        $heading.each(function () {
-          var $this = $(this);
-          if (($this.is('h1, h2, h3, h4, h5, h6'))) {
-            $this.attr('aria-level', '3');
-          } else {
-            $this.attr({
-              'role': 'heading',
-              'aria-level': '3'
-            });
-          }
-        });
+        if ($selector.is('div')) {
+          $selector.find('h3').attr({
+            'aria-level': 3,
+            'aria-controls': controls
+          });
+        } else {
+          $heading.each(function () {
+            var $this = $(this);
+            if (($this.is('h1, h2, h3, h4, h5, h6'))) {
+              $this.attr('aria-level', '3');
+            } else {
+              $this.attr({
+                'role': 'heading',
+                'aria-level': '3'
+              });
+            }
+          });
 
-        // 버튼, a태그 체크
-        $trigger.each(function () {
-          var $this = $(this);
-          if ($this.is('a')) {
-            $this.attr({
-              'role': 'button',
-              'href': '#' + $this.attr('aria-controls'),
-              'aria-controls': $panel.attr('id'),
-              'aria-expanded': false
-            });
-          } else if ($this.is('button')) {
-            $this.attr({
-              'aria-expanded': false,
-              'aria-controls': $panel.attr('id')
-            });
-          }
-        });
+          // 버튼, a태그 체크
+          $trigger.each(function () {
+            var $this = $(this);
+            if ($this.is('a')) {
+              $this.attr({
+                'role': 'button',
+                'href': '#' + $this.attr('aria-controls'),
+                'aria-controls': $panel.attr('id'),
+                'aria-expanded': false
+              });
+            } else if ($this.is('button')) {
+              $this.attr({
+                'aria-expanded': false,
+                'aria-controls': $panel.attr('id')
+              });
+            }
+          });
+        }
+
 
         $panel.attr({
           'aria-labelledby': labelledby,
@@ -52,24 +85,28 @@
         });
 
         // 기본으로 펼쳐져있는 것 체크
-        if ($selector.find('.' + activate).length && $($selector.find('.' + activate).attr('href')).css('display') === 'block') {
-          $selector.find('.' + activate).attr('aria-expanded', true);
+        if ($selector.find('.' + activeClass).length && $($selector.find('.' + activeClass).attr('href')).css('display') === 'block') {
+          $selector.find('.' + activeClass).attr('aria-expanded', true);
         }
 
         eventAction();
       };
+
+      var _local = function() {
+
+      },
 
       var eventAction = function () {
         $trigger.on('click', function (e) {
           var _this = $(this),
             _target = '#' + _this.attr('aria-controls');
 
-          if (!_this.hasClass(activate) && $(_target).css('display') === 'none') {
+          if (!_this.hasClass(activeClass) && $(_target).css('display') === 'none') {
             // 닫기
-            $trigger.removeClass(activate).attr('aria-expanded', false);
+            $trigger.removeClass(activeClass).attr('aria-expanded', false);
             $panel.css('display', 'none');
             // 열기
-            _this.addClass(activate).attr('aria-expanded', true);
+            _this.addClass(activeClass).attr('aria-expanded', true);
             $(_target).css('display', 'block');
 
             // 화면 이동
@@ -79,7 +116,7 @@
               }, 800);
             }
           } else {
-            _this.removeClass(activate).attr('aria-expanded', false);
+            _this.removeClass(activeClass).attr('aria-expanded', false);
             $(_target).css('display', 'none');
           }
 
@@ -106,4 +143,6 @@
     // Callbacks
     activate: null
   };
+
+
 })(window, document, jQuery);
